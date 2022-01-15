8fe89f55a714e1d714fa4857878dca857724b3b4
Freddy <freddyvelarde59@gmail.com>
2022-01-15 17:25:47 -0400

    double code fixed, JWT and Bcryptjs added and configured

--- src/index.js
+++ src/index.js
@@ -24,6 +24,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`)
-app.listen(app.get('port'), () => {
-  console.log(`the server is listening on port http://localhost:${app.get('port')}`)
 })
