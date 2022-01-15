f4cc82254daf219641630b69dedd47c563679b06
Freddy <freddyvelarde59@gmail.com>
2022-01-15 15:19:05 -0400

    I've been added files to ordened the project

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const cors =  require('cors');
+
 // middlewares
-app.use(express.json()); // code to express understand json objects
-app.use(cors()); // cors, to avoid problems in the future with the front end.
+app.use(express.json()); // cuz express dosen't understand json objects.
+app.use(cors()); // cors, to avoid problems in the future with the frontend.
+
 // config
 app.set('port', process.env.PORT || 8000);
+
+// here is the Data Base. you need to set up your mongodb in your computer.
+require('./database');  // to configure only write "mongod" in another terminal.
+
+// Routers
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-// here is the Data Base. you need to set up your mongodb in your computer.
-// to configure only write "mongod" in another terminal.
-require('./database');
-
 app.listen(app.get('port'), () => {
-  console.log(`Example app listening at http://localhost:${app.get('port')}`)
+  console.log(`the server is listening on port http://localhost:${app.get('port')}`)
 })
